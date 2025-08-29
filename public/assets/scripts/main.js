adbae628a492f05fda87a3838d72a75329bb9f5e
David Stratton <me@davidstratton.co.uk>
2025-08-29 16:24:44 +0100

    - Added New User Dialog - Added Switch User Dialog - Added Buttons to show new and switch user dialog. - Added Javascript to list users from local storage - Added Javascript to generate options for switch user dialog based on user list. - Added Javascript to Add new users to local storage

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,6 +1,3 @@
-const limit = 100;
-const url = `https://picsum.photos/v2/list?limit=${limit}`;
-
 function checkStatus(response) {
   if (response.ok) {
     return Promise.resolve(response);
@@ -29,13 +26,6 @@ function generateImage(data) {
     image.setAttribute("data-id", data[number].id );
     
     currentImage.replaceWith(image);
-}
-
-let currentUser = localStorage.getItem('currentUser') === null ? "guest" : localStorage.getItem('currentUser');
-const userLabel = document.querySelector("h2 span.user");
-
-if (currentUser !== null) {
-    userLabel.textContent = currentUser;
 }
 
 const randomImage = () => {
@@ -97,18 +87,113 @@ const showCollection = () => {
         itemImg.setAttribute("src", val.download_url);
         itemImg.setAttribute("alt", " ");
         itemImg.setAttribute("data-id", key );
-        itemLi.append(itemImg);
-
+        
         itemDiv = document.createElement("div");
         itemDiv.classList.add("item-remove");
-        itemImg.append(itemDiv);
 
         itemIcon = document.createElement("i");
         itemIcon.textContent = "X";
+        
         itemDiv.append(itemIcon);
-
-        collectionList.apprend(itemLi);
+        itemLi.append(itemImg, itemDiv);
+        collectionList.append(itemLi);
     });
+}
+
+function getUserList() {
+    if (!localStorage.getItem("userList")) {
+        console.log(`error: No userList Found`);
+        let userList = ["Guest"];
+        localStorage.setItem("userList", JSON.stringify(userList));
+        return userList;
+    } 
+    return JSON.parse(localStorage.getItem("userList"));
+}
+
+function listUsers() {
+    let userList = getUserList();
+
+    let options = document.querySelector("#switch-user");
+
+    for ( i = 1; i < userList.length; i++) {
+        let option = document.createElement("option");
+        option.value = userList[i];
+        option.text = userList[i];
+        options.add(option);
+    }
+}
+
+const switchUser = () => {
+    console.log("User Switch Performed");
+    //set currentUser to new user
+    //switch header to new user
+    //switch collection header to new user
+    //load new empty collection
+}
+
+// Could Fit these functions together.
+const formSubmitNew = (event) => {
+    event.preventDefault();
+
+    // Get Username
+    username = formNewUser.querySelector("#new-email").value;
+    
+    // Get User List
+    if (localStorage.getItem["userList"]) {
+        userList = JSON.parse(localStorage.getItem["userList"]);
+    }
+
+    // Check if User exists
+    if (userList.includes(username)) {
+        return console.log("Error: User Already Exists!");
+    }
+
+    // Add New User to userlist
+    userList.push(username);
+    localStorage.setItem("userList", JSON.stringify(userList));
+
+    // Update User Selection Option
+    let options = document.querySelector("#switch-user");
+    let option = document.createElement("option");
+    option.value = username;
+    option.text = username;
+    options.add(option);
+
+    // Perform User Switch
+    switchUser(username);
+
+    //close dialog
+    dialogNewUser.close();
+
+    //notify user of success
+}
+
+const formSubmitSwitch = (event) => {
+    event.preventDefault();
+
+    //grab form input for new user name
+    switchUser(username);
+
+    //close dialog
+    dialogSwitchUser.close();
+
+    //notify user of success
+}
+
+const onLoadHandler = () => {
+    listUsers();
+    randomImage();
+    showCollection();
+}
+
+const limit = 100;
+const url = `https://picsum.photos/v2/list?limit=${limit}`;
+
+let currentUser = localStorage.getItem('currentUser') === null ? "guest" : localStorage.getItem('currentUser');
+const userLabel = document.querySelector("h2 span.user");
+
+if (currentUser !== null) {
+    userLabel.textContent = currentUser;
 }
 
 let collection = {};
@@ -117,8 +202,25 @@ const imageRandomButton = document.querySelector(".image-new");
 const imageAddButton = document.querySelector(".image-add");
 const collectionList = document.querySelector(".collection-list");
 
+const dialogNewUser = document.querySelector("#dialog-new");
+const buttonNewOpen = document.querySelector("#button-new-open");
+const buttonNewClose = document.querySelector("#button-new-close");
+const formNewUser = document.querySelector("#form-new");
+const buttonNewSubmit = document.querySelector("#button-new-submit");
+buttonNewOpen.addEventListener("click", () => dialogNewUser.showModal());
+buttonNewClose.addEventListener("click", () => dialogNewUser.close());
+buttonNewSubmit.addEventListener("click", formSubmitNew);
 
-document.addEventListener('DOMContentLoaded', randomImage);
-document.addEventListener('DOMContentLoaded', showCollection);
+const dialogSwitchUser = document.querySelector("#dialog-switch");
+const buttonSwitchOpen = document.querySelector("#button-switch-open");
+const buttonSwitchClose = document.querySelector("#button-switch-close");
+const formSwitchUser = document.querySelector("#form-switch");
+const buttonSwitchSubmit = document.querySelector("#button-switch-submit");
+buttonSwitchOpen.addEventListener("click", () => dialogSwitchUser.showModal());
+buttonSwitchClose.addEventListener("click", () => dialogSwitchUser.close());
+buttonSwitchSubmit.addEventListener("click", formSubmitSwitch);
+
+document.addEventListener('DOMContentLoaded', onLoadHandler);
+
 imageRandomButton.addEventListener("click", randomImage);
 imageAddButton.addEventListener("click", addImage);
