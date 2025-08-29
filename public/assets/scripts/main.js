bb3b7ce91c9247f96591701c1e16078df3d8ad14
David Stratton <me@davidstratton.co.uk>
2025-08-29 12:39:20 +0100

    - Added Javascript to add the current image to users  collection of images. - Added Javascript to display a users collection of images.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -26,11 +26,12 @@ function generateImage(data) {
     let image = document.createElement("img");
     image.setAttribute("src", data[number].download_url);
     image.setAttribute("alt", " ");
+    image.setAttribute("data-id", data[number].id );
     
     currentImage.replaceWith(image);
 }
 
-const currentUser = localStorage.getItem('currentUser') === null ? null : localStorage.getItem('currentUser');
+let currentUser = localStorage.getItem('currentUser') === null ? "guest" : localStorage.getItem('currentUser');
 const userLabel = document.querySelector("h2 span.user");
 
 if (currentUser !== null) {
@@ -39,9 +40,7 @@ if (currentUser !== null) {
 
 const randomImage = () => {
 
-    console.log(`Random Image!`);
     if (localStorage.getItem('imageList')) {
-        console.log(`Local List Random Image!`);
         let imageList = JSON.parse(localStorage.getItem('imageList'));
         return generateImage(imageList);
     }
@@ -53,7 +52,73 @@ const randomImage = () => {
         });
 }
 
-const imageButton = document.querySelector(".image-new");
+const addImage = () => {
+
+let imageList = JSON.parse(localStorage.getItem('imageList'));
+
+let currentImage = document.querySelector(".image-current").firstElementChild;
+let currentImageId = currentImage.getAttribute("data-id");
+
+    if (!currentUser) {
+        // this shouldn't be possible for failsafe anyway
+        let currentUser = "guest";
+    }
+
+    if (localStorage.getItem(currentUser)) {
+        console.log(`Current Collection found for Current User`);
+        collection = JSON.parse(localStorage.getItem(currentUser));
+    }
+
+    collection[currentImageId] = imageList[currentImageId];
+    localStorage.setItem(currentUser, JSON.stringify(collection));
+    console.log(`Image Added to ${currentUser}'s collection`)
+}
+
+const showCollection = () => {
+    
+    if (!currentUser) {
+        // this shouldn't be possible for failsafe anyway
+        let currentUser = "guest";
+    }
+
+    if (!localStorage.getItem(currentUser)) {
+        return console.log(`No Collection found for Current User`);
+    }
+
+    console.log(`Current Collection found for Current User`);
+    console.log(`current user: ${currentUser}`);
+    collection = JSON.parse(localStorage.getItem(currentUser));
+    Object.entries(collection).forEach(([key, val]) => {
+        itemLi = document.createElement("li");
+        itemLi.setAttribute("data-id", key);
+        itemLi.classList.add("collection-item");
+
+        itemImg = document.createElement("img");
+        itemImg.setAttribute("src", val.download_url);
+        itemImg.setAttribute("alt", " ");
+        itemImg.setAttribute("data-id", key );
+        itemLi.append(itemImg);
+
+        itemDiv = document.createElement("div");
+        itemDiv.classList.add("item-remove");
+        itemImg.append(itemDiv);
+
+        itemIcon = document.createElement("i");
+        itemIcon.textContent = "X";
+        itemDiv.append(itemIcon);
+
+        collectionList.apprend(itemLi);
+    });
+}
+
+let collection = {};
+
+const imageRandomButton = document.querySelector(".image-new");
+const imageAddButton = document.querySelector(".image-add");
+const collectionList = document.querySelector(".collection-list");
+
 
 document.addEventListener('DOMContentLoaded', randomImage);
-imageButton.addEventListener("click", randomImage);
+document.addEventListener('DOMContentLoaded', showCollection);
+imageRandomButton.addEventListener("click", randomImage);
+imageAddButton.addEventListener("click", addImage);
